b7f23e9d4faad5eeb1b57bfb7cfd715d61f4063b
Brian Nyamweya <briangnyamweya@gmail.com>
2023-04-27 16:04:59 +0300

    minor

--- app/js/style.js
+++ app/js/style.js
@@ -1,1 +1,8 @@
 // ScrollReveal().reveal("section", {delay:100 ,easing:"ease-out" ,interval: 200 ,scale: 1.2 });
+
+let typed = new Typed("auto-type",{
+    strings : ["your online presence", "with","our","expert"," web development solutions"],
+    typeSpeed: 150,
+    backSpeed : 150,
+    looped : true
+})
