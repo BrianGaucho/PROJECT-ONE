fa4c90203344b1d25bc41604df6a585febdb49af
Brian Nyamweya <briangnyamweya@gmail.com>
2023-04-27 16:33:10 +0300

    typed js

--- app/js/style.js
+++ app/js/style.js
@@ -1,8 +1,17 @@
 // ScrollReveal().reveal("section", {delay:100 ,easing:"ease-out" ,interval: 200 ,scale: 1.2 });
 
-let typed = new Typed("auto-type",{
-    strings : ["your online presence", "with","our","expert"," web development solutions"],
-    typeSpeed: 150,
-    backSpeed : 150,
-    looped : true
-})
+// let typed = new Typed("auto-type",{
+//     strings : ["your online presence", "with","our","expert"," web development solutions"],
+//     typeSpeed: 150,
+//     backSpeed : 150,
+//     looped : true
+// })
+
+// import Typed from 'typed.js';
+
+const typed = new Typed('#element', {
+  strings: ['<i>Empower</i> your online presence with our', 'web development solutions'],
+  typeSpeed: 150,
+  backSpeed : 150,
+  looped : true,  
+});
